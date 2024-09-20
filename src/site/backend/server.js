4ba6b830fd34a599b8f89ac25010b5db47f7616b
Kamikoto-Mayer <danielmayer202@gmail.com>
2024-09-20 10:34:43 +0300

    Fri Sep 20 10:34:43 MSK 2024

--- src/site/backend/server.js
+++ src/site/backend/server.js
@@ -1,5 +1,37 @@
-const dotenv = require('dotenv');
 const express = require('express');
-const axios = require('axios');
-dotenv.config();
+const axois = require('axios');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+const PORT = 2099;
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+TELEGRAM_BOT_TOKEN = "7359250227:AAGU4m4ORXFtph_KkSaZiKD1Ag5W83p2H-g";
+TEKEGRAM_CHAT_ID = "-4565200718";
+
+app.post('/send-message', (req, res) => {
+	const { name, email, message } = req.body;
+	const text = `Заявка с сайта\nИмя: ${name}\nEmail: ${email}\nСообщение: ${message}`;
+	try  {
+		axois.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
+			chat_id: TEKEGRAM_CHAT_ID,
+			text: text
+		})
+	} catch (error) {
+		console.error('Error sending message:', error);
+	}
+	res.status('200').json({ message: "Сервер работает!" })
+})
+
+app.get('/send-message', (req, res) => {
+	res.status('200').json({ message: "Сервер работает!" })
+})
+
+
+
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`)
+})
--- src/site/frontend/src/components/form/form.tsx
+++ src/site/frontend/src/components/form/form.tsx
@@ -1,6 +1,33 @@
 import "./form.css"
+import axios from "axios"
+import { useState } from "react"
+
+
 
 export default function Form() {
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		message: ''
+	  });
+
+	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setFormData({
+		  ...formData,
+		  [name]: value
+		});
+	};
+
+	const handleSubmit = async () => {
+		try {
+		  await axios.post('http://localhost:2099/send-message' , formData);
+		  setFormData({ name: '', email: '', message: '' });
+		} catch (error) {
+		  console.error('Error sending message:', error);
+		}
+	};
+
 	return (
 		<section className="form">
 			<div className="container">
@@ -9,18 +36,18 @@ export default function Form() {
 						<h2>Связаться со мной</h2>
 					</div>
 					<div className="form__form">
-						<form action="#" className="form__forms">
+						<form action="#" onSubmit={handleSubmit} className="form__forms">
 							<div className="form__input form__input-name">
 								<label htmlFor="name" className="form__label">Имя</label>
-								<input type="text" name="name" id="name" placeholder="Твое Имя" required autoComplete="on" />
+								<input type="text" name="name" value={formData.name} onChange={handleChange} id="name" placeholder="Твое Имя" required  />
 							</div>
 							<div className="form__input form__input-email">
 								<label htmlFor="email" className="form__label">Почта</label>
-								<input type="text" name="email" id="email" placeholder="email@example.com" required autoComplete="on" />
+								<input type="email" name="email" value={formData.email} onChange={handleChange} id="email" placeholder="email@example.com" required />
 							</div>
 							<div className="form__input form__input-message">
 								<label htmlFor="message" className="form__label">Сообщение</label>
-								<textarea name="message" id="message" placeholder="Сообщение" rows={1} cols={1} required/>
+								<textarea name="message" id="message" value={formData.message} onChange={handleChange} placeholder="Сообщение" rows={1} cols={1} required/>
 							</div>
 							<div className="form__button button">
 								<button className="button-form button-item" type="submit">Отправить</button>
